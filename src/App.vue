3a0bddad30dfb4c7f9eb57d3e65bdaea5ec705fc
coderycs <1748780248@qq.com>
2022-11-30 21:47:09 +0800

    feat: 添加store

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="app">
+    <h2>{{ $store.state.name }}</h2>
     <router-link to="/login">登录</router-link>
     <router-link to="/main">首页</router-link>
     <router-view></router-view>
---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+
+const store = createStore({
+  state: () => {
+    return {
+      name: 'coderycs'
+    }
+  },
+  mutations: {},
+  getters: {}
+})
+
+export default store
