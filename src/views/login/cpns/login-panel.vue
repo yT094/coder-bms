32b8c81f76b0aec6303be67a3629ad03d64850d6
coderycs <1748780248@qq.com>
2022-12-04 13:55:10 +0800

    feat: 添加tab切换识别

--- src/views/login/cpns/login-panel.vue
+++ src/views/login/cpns/login-panel.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="login-panel">
     <h2 class="l-title">后台管理系统</h2>
-    <el-tabs type="border-card" stretch>
-      <el-tab-pane>
+    <el-tabs type="border-card" stretch v-model="currentTab">
+      <el-tab-pane name="account">
         <template #label>
           <span class="custom-tabs-label">
             <span><i class="el-icon-user-solid"></i> 账号登录</span>
@@ -11,7 +11,7 @@
         <!-- 组件实例: 组件的工作方式与类有点相似 -->
         <login-account ref="accountRef" />
       </el-tab-pane>
-      <el-tab-pane>
+      <el-tab-pane name="phone">
         <template #label>
           <span class="custom-tabs-label">
             <span><i class="el-icon-mobile-phone"></i> 手机登录</span>
@@ -42,13 +42,22 @@ import LoginPhone from './login-phone.vue'
 export default defineComponent({
   components: { LoginAccount, LoginPhone },
   setup() {
+    // 1.定义属性
     const isKeepPassword = ref(true)
     const accountRef = ref<InstanceType<typeof LoginAccount>>() // 获取组件的类型
+    const phoneRef = ref<InstanceType<typeof LoginPhone>>()
+    const currentTab = ref('account')
+
+    // 2.定义方法
     const onLoginBtn = () => {
       console.log('监听登录按钮')
-      accountRef.value?.loginAction(isKeepPassword.value)
+      if (currentTab.value === 'account') {
+        accountRef.value?.loginAction(isKeepPassword.value)
+      } else {
+        console.log('phoneRef调用loginAction')
+      }
     }
-    return { isKeepPassword, onLoginBtn, accountRef }
+    return { isKeepPassword, onLoginBtn, accountRef, phoneRef, currentTab }
   }
 })
 </script>
