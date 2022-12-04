bdd9c221d4c17eaa04345ee0584d3346dea6b8dd
coderycs <1748780248@qq.com>
2022-12-04 17:11:25 +0800

    feat: 将token写入vuex和localStorage中
    说明：一旦用户重新登录，需要及时更新vuex和localStorage中的token值

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -4,6 +4,9 @@ import { accountLoginRequest } from '@/service/login/login'
 
 import { ILoginState } from './types'
 import { IRootState } from '../types'
+import { IAccount } from '@/service/login/type'
+
+import localCache from '@/utils/cache'
 
 const loginModule: Module<ILoginState, IRootState> = {
   namespaced: true,
@@ -14,12 +17,19 @@ const loginModule: Module<ILoginState, IRootState> = {
     }
   },
   getters: {},
-  mutations: {},
+  mutations: {
+    changeToken(state, token: string) {
+      state.token = token
+    }
+  },
   actions: {
-    async accountLoginAction({ commit }, payload: any) {
+    async accountLoginAction({ commit }, payload: IAccount) {
+      // 1.实现登录逻辑
       const loginResult = await accountLoginRequest(payload)
       const { id, token } = loginResult.data
-      console.log('loginResult', id, token)
+      // 保存token值到vuex和localStorage中
+      commit('changeToken', token)
+      localCache.setCache('token', token)
     }
   }
 }
