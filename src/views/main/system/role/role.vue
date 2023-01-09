99b3ab8a1591955f63e039393149f4416afbd36e
coderycs <1748780248@qq.com>
2023-01-09 20:22:21 +0800

    feat: 角色管理添加弹框组件

--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -12,6 +12,12 @@
       @addBtnClick="handleAddBtnClick"
       @editBtnClick="handleEditBtnClick"
     />
+    <page-modal
+      pageName="role"
+      ref="pageModalRef"
+      :dialogFormConfig="dialogFormConfig"
+      :defaultInfo="defaultInfo"
+    />
   </div>
 </template>
 
@@ -20,28 +26,39 @@ import { defineComponent } from 'vue'
 
 import PageSearch from '@/components/page-search'
 import PageContent from '@/components/page-content'
+import PageModal from '@/components/page-modal'
 
 import { searchFormConfig } from './config/search-config'
 import { contentTableConfig } from './config/content-config'
+import { dialogFormConfig } from './config/modal-config'
 
 import { usePageSearch } from '@/hooks/use-page-search'
+import { usePageModal } from '@/hooks/use-page-modal'
 
 export default defineComponent({
   components: {
     PageSearch,
-    PageContent
+    PageContent,
+    PageModal
   },
   name: 'role',
   setup() {
     const [pageContentRef, handleResetBtnClick, handleQueryBtnClick] =
       usePageSearch()
+    const [defaultInfo, pageModalRef, handleAddBtnClick, handleEditBtnClick] =
+      usePageModal()
 
     return {
       searchFormConfig,
       contentTableConfig,
+      dialogFormConfig,
       pageContentRef,
       handleQueryBtnClick,
-      handleResetBtnClick
+      handleResetBtnClick,
+      defaultInfo,
+      pageModalRef,
+      handleAddBtnClick,
+      handleEditBtnClick
     }
   }
 })
