e321bd9aa8706ebb61880e720addb41d3ea92928
coderycs <1748780248@qq.com>
2023-01-12 10:16:24 +0800

    feat: 封装base-echart

---
+++ src/base-ui/echarts/hooks/useEchart.ts
@@ -0,0 +1,18 @@
+import * as echarts from 'echarts'
+
+export default function (el: HTMLElement) {
+  const echartInstance = echarts.init(el)
+
+  const setOptions = (options: echarts.EChartsOption) => {
+    echartInstance.setOption(options)
+  }
+
+  window.addEventListener('resize', () => {
+    echartInstance.resize()
+  })
+
+  return {
+    echartInstance,
+    setOptions
+  }
+}
---
+++ src/base-ui/echarts/src/base-echart.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="base-echart">
+    <div
+      class="echart"
+      ref="echartDivRef"
+      :style="{ width: width, height: height }"
+    ></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted, defineProps, withDefaults, watchEffect } from 'vue'
+import useEchart from '../hooks/useEchart'
+import { EChartsOption } from 'echarts'
+
+const props = withDefaults(
+  defineProps<{
+    options: EChartsOption
+    width?: string
+    height?: string
+  }>(),
+  {
+    options: () => ({}),
+    width: '100%',
+    height: '350px'
+  }
+)
+
+const echartDivRef = ref<HTMLElement>()
+
+onMounted(() => {
+  const [setOptions] = useEchart(echartDivRef.value!)
+  watchEffect(() => {
+    setOptions(props.options)
+  })
+})
+</script>
+
+<style lang="less" scoped>
+.echart {
+  background-color: #fff;
+}
+</style>
